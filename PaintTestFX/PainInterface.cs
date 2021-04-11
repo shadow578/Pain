29f87d51f65ccbe65c2acef474d408df5265e826
shadow578 <christophnienhaus2@gmail.com>
2021-04-11 16:09:16 +0200

    friendship with SVG ended

--- PaintTestFX/PainInterface.cs
+++ PaintTestFX/PainInterface.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
 using System.Threading;
-using System.Windows.Media;
-
-using WPoint = System.Windows.Point;
 
 namespace PaintTestFX
 {
@@ -27,63 +23,6 @@ namespace PaintTestFX
         /// enable command logging to console
         /// </summary>
         public bool EnableCW { get; set; } = false;
- 
-        // region SVG Path
-
-        /// <summary>
-        /// attempt to draw a svg path (uses WPF geometry under the hood)
-        /// </summary>
-        /// <param name="path">the path to draw</param>
-        /// <param name="width">the viewport width</param>
-        /// <param name="height">the viewport height</param>
-        public void DrawPath(string path, int width, int height)
-        {
-            CW("CMD: DrawPath");
-
-            // parse using WPF
-            Geometry geometry = Geometry.Parse(path);
-            PathGeometry pathGeometry = PathGeometry.CreateFromGeometry(geometry);
-
-            // every figure
-            foreach (PathFigure fig in pathGeometry.Figures)
-            {
-                // every segement
-                foreach (PathSegment seg in fig.Segments)
-                {
-                    CW($"Segment: {seg.GetType().Name}");
-                    DrawPolyBezier(seg as PolyBezierSegment, width, height);
-                }
-            }
-        }
-
-        /// <summary>
-        /// draw a poly bezier (badly, that is)
-        /// </summary>
-        /// <param name="seg">the segment to draw</param>
-        /// <param name="width">the viewport width</param>
-        /// <param name="height">the viewport height</param>
-        void DrawPolyBezier(PolyBezierSegment seg, int width, int height)
-        {
-            // check the segment is valid
-            if (seg == null)
-                return;
-
-            // prepare polygon to draw
-            PointF[] poly = seg.Points.Select(p =>
-            {
-                // normalize point
-                p.X /= width;
-                p.Y /= height;
-
-                // convert to PointF
-                return ToPoint(p);
-            }).ToArray();
-
-            // draw polygon closed
-            DrawPolygon(verticies: poly);
-        }
-
-        //endregion
 
         /// <summary>
         /// draw a polygon
@@ -118,7 +57,7 @@ namespace PaintTestFX
             Cursor.LMBUp();
         }
 
-        //region util
+        #region util
         /// <summary>
         /// movement delay
         /// </summary>
@@ -147,16 +86,6 @@ namespace PaintTestFX
             y += Bounds.Y;
 
             return new Point(x, y);
-        }
-
-        /// <summary>
-        /// convert a WPF point to a Forms point
-        /// </summary>
-        /// <param name="point">the wpf point</param>
-        /// <returns>the forms point</returns>
-        PointF ToPoint(WPoint point)
-        {
-            return new PointF((float)point.X, (float)point.Y);
         }
 
         /// <summary>
@@ -202,9 +131,9 @@ namespace PaintTestFX
         /// <param name="msg">message to write</param>
         void CW(string msg)
         {
-            if(EnableCW)
+            if (EnableCW)
                 Console.WriteLine(msg);
         }
-        //endregion
+        #endregion
     }
 }
